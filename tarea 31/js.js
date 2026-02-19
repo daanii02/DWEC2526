8451b222c1e4bd0d04bd222b037107ac7afeca37
DANI <galanredondo.daniel@jgvaldemora.org>
2026-02-19 10:31:22 +0100

    31

--- tarea 31/js.js
+++ tarea 31/js.js
@@ -1,26 +1,24 @@
 let padre = document.getElementById("padre");
-let fortuna=document.createElement("input");
-fortuna.setAttribute("type","button");
-fortuna.setAttribute("value", "");
-fortuna.className="boton";
-fortuna.addEventListener("click",function(){
- 
-    let frases=[
-        "Mas vale tarde que nunca.",
-        "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
-        "No cuentes los días, haz que los días cuenten.",
-        "El único modo de hacer un gran trabajo es amar lo que haces.",
-        "La felicidad no es algo hecho. Viene de tus propias acciones."
-    ];
-    for (let i=0;i<frases.length;i++){
-        let indice=Math.floor(Math.random()*frases.length);
-        document.className(".frase").innerHTML=frases[indice];
-        
-    }
 
-}); 
+let fortuna = document.createElement("input");
+fortuna.setAttribute("type", "button");
+fortuna.setAttribute("value", "");
+fortuna.className = "boton";
 padre.appendChild(fortuna);
 
-let p = document.createElement("p");
-p.className="frase";
-padre.appendChild(p);
+let salida = document.createElement("p");
+salida.className = "frase";
+padre.appendChild(salida);
+
+fortuna.addEventListener("click", function () {
+  let frases = [
+    "Mas vale tarde que nunca.",
+    "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
+    "No cuentes los días, haz que los días cuenten.",
+    "El único modo de hacer un gran trabajo es amar lo que haces.",
+    "La felicidad no es algo hecho. Viene de tus propias acciones."
+  ];
+
+  let indice = Math.floor(Math.random() * frases.length);
+  salida.textContent = frases[indice];
+});
